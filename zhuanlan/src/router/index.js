110037fd459a71cf62e68e2f3feb88162be94caf
hrpc <1131197290@qq.com>
2017-12-15 18:36:02 +0800

    first
    asd

---
+++ zhuanlan/src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+const Index = () => import('@/components/Index')
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      redirect: '/index'
+    },
+    {
+      path: '/index',
+      component: Index
+    }
+  ]
+})
