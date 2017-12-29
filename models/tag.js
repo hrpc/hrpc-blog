ab018c61537019b27550e4bfb1159a6e11267b9a
hrpc <1131197290@qq.com>
2017-12-29 15:03:18 +0800

    123

--- models/tag.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('../db/mongo');
-const Schema = mongoose.Schema;
-const tagSchema = new Schema({
-	name:String,
-	default:''
-})
-//发布该框架生成model
-module.exports = mongoose.model('tag',tagSchema)
